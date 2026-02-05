d3de33f9e20f177e4af39a5f6e6b33042de0de27
MUSHAM GANESH <mushamganeshnetha7172@gmail.com>
2026-02-05 08:38:23 +0530

    Time: 4 ms (31.61%) | Memory: 12.6 MB (23.23%) - LeetSync

---
+++ 2401-count-asterisks/count-asterisks.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countAsterisks(self, s):
+        inside = False
+        count = 0
+
+        for c in s:
+            if c == '|':
+                inside = not inside
+            elif c == '*' and not inside:
+                count += 1
+
+        return count
