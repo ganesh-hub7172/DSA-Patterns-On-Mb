9217fa5da4fb5ee4c95ece0ad1b7be5b712bf224
MUSHAM GANESH <mushamganeshnetha7172@gmail.com>
2026-02-27 07:23:14 +0530

    Time: 31 ms (75.70%) | Memory: 14.2 MB (52.51%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findContentChildren(self, g, s):
+        g.sort()
+        s.sort()
+        
+        child = 0
+        cookie = 0
+        
+        while child < len(g) and cookie < len(s):
+            if s[cookie] >= g[child]:
+                child += 1   # child is satisfied
+            cookie += 1      # move to next cookie
+        
+        return child
